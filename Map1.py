114f2390a5a7010f3105d1ac266a82e6ced301d7
asa416 <69068237+asa416@users.noreply.github.com>
2021-10-23 19:50:05 +0900

    coin add

--- Map1.py
+++ Map1.py
@@ -10,6 +10,9 @@ treePos = [100, 600, 1100, 1800]
 grassPos = [150, 650, 1150, 1850]
 goomPos = [300, 3000, 3300, 6000, 7000, 7300, 8000]
 turtlePos = [1000, 1100, 2300, 2400, 5500, 7500]
+block2Pos = [500, 550, 600, 650]
+pipePos = [3000, 5200]
+coinPos = [500, 550, 600, 650]
 
 class Map1:
     def __init__(self):
@@ -25,6 +28,9 @@ class Map1:
         self.grassNum = len(grassPos)
         self.gooms = [enemy.Goom(goomPos[i], 105) for i in range(len(goomPos))]
         self.turtles = [enemy.Turtle(turtlePos[i], 115) for i in range(len(turtlePos))]
+        self.block2s = [object.UneasyBlock(block2Pos[i], 250) for i in range(len(block2Pos))]
+        self.pipes = [object.Pipe(pipePos[i]) for i in range(len(pipePos))]
+        self.coins = [object.Coin(coinPos[i], 300) for i in range(len(coinPos))]
 
     def set_camera(self, c):
         self.camera = c
@@ -32,12 +38,20 @@ class Map1:
             goom.set_camera(self.camera)
         for turtle in self.turtles:
             turtle.set_camera(self.camera)
+        for block2 in self.block2s:
+            block2.set_camera(self.camera)
+        for pipe in self.pipes:
+            pipe.set_camera(self.camera)
+        for coin in self.coins:
+            coin.set_camera(self.camera)
 
     def update(self):
         for goom in self.gooms:
             goom.update()
         for turtle in self.turtles:
             turtle.update()
+        for coin in self.coins:
+            coin.update()
         self.back.update()
 
     def draw(self):
@@ -54,6 +68,12 @@ class Map1:
         for turtle in self.turtles:
             turtle.draw()
         self.back.draw()
+        for block2 in self.block2s:
+            block2.draw()
+        for pipe in self.pipes:
+            pipe.draw()
+        for coin in self.coins:
+            coin.draw()
 
 if __name__ == '__main__':
     open_canvas()
--- object.py
+++ object.py
@@ -1,13 +1,13 @@
 from pico2d import *
 
 class Block:
-    lmage = None
+    image = None
     x, y = 100, 100
     w, h = 50, 50
     state = False
     camera = 0
 
-    def get_camera(self, c):
+    def set_camera(self, c):
         self.camera = c
 
     def update(self):
@@ -17,12 +17,13 @@ class Block:
         return self.x - self.w / 2, self.y - self.h / 2, self.x + self.w / 2, self.y + self.h / 2
 
     def draw(self):
-        self.image.draw(self.x - self.camer, self.y - self.camera, self.w, self.h)
-        draw_rectangle(self.x - self.w / 2 - self.camera, self.y - self.h / 2 - self.camera, self.x + self.w / 2 - self.camera, self.y + self.h / 2 - self.camera)
+        self.image.draw(self.x - self.camera, self.y, self.w, self.h)
+        draw_rectangle(self.x - self.w / 2 - self.camera, self.y - self.h / 2, self.x + self.w / 2 - self.camera, self.y + self.h / 2)
 
 class RandomBox(Block):
-    def __init__(self):
+    def __init__(self, x, y):
         self.image = load_image('randombox.png')
+        self.x, self.y = x, y
         state = True
         self.num = 1
         # self.item = 1
@@ -31,21 +32,53 @@ class RandomBox(Block):
         if self.num != 0:
             pass
 
+class UneasyBlock(Block):
+    def __init__(self, x, y):
+        self.image = load_image('block2.png')
+        self.x, self.y = x, y
+        self.frame = 0
 
+class EasyBlock(Block):
+    def __init__(self):
+        self.image = load_image('block3.png')
 
 class Pipe:
-    def __init__(self, x, y):
+    def __init__(self, x):
         self.image = load_image('pipeline.png')
-        self.x, self.y = x, y
+        self.x = x
+        self.y = 70
         self.xsize, self.ysize = 66, 140
         self.camera = 0
 
-    def get_camera(self, c):
+    def set_camera(self, c):
         self.camera = c
 
     def get_bb(self):
-        return self.x - self.xsize / 2, self.y - self.ysize / 2, self.x + self.xsize / 2, self.y + self.ysize / 2
+        return self.x - self.xsize / 2 - self.camera, self.y - self.ysize / 2, self.x + self.xsize / 2 - self.camera, self.y + self.ysize / 2
 
     def draw(self):
-        self.image.draw(self.x - self.camera, self.y - self.camera)
-        draw_rectangle(self.x - self.xsize / 2 - self.camera, self.y - self.ysize / 2 - self.camera, self.x + self.xsize / 2 - self.camera, self.y + self.ysize / 2 - self.camera)
+        self.image.draw(self.x - self.camera, self.y)
+        draw_rectangle(self.x - self.xsize / 2 - self.camera, self.y - self.ysize / 2, self.x + self.xsize / 2 - self.camera, self.y + self.ysize / 2)
+
+class Coin:
+    image = None
+    w, h = 30, 30
+    camera = 0
+    frame = 0
+
+    def __init__(self, x, y):
+        self.image = load_image('coin.png')
+        self.x, self.y = x, y
+
+    def set_camera(self, c):
+        self.camera = c
+
+    def update(self):
+        self.frame = (self.frame + 1) % 60
+
+    def get_bb(self):
+        return self.x - self.w / 2 - self.camera, self.y - self.h / 2, self.x + self.w / 2 - self.camera, self.y + self.h / 2
+
+    def draw(self):
+        self.image.clip_draw(120 * (self.frame // 10), 0, 120, 115, self.x - self.camera, self.y, self.w, self.h)
+        draw_rectangle(self.x - self.w / 2 - self.camera, self.y - self.h / 2, self.x + self.w / 2 - self.camera, self.y + self.h / 2)
